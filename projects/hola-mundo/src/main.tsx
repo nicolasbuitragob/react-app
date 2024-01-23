5db1f095a1cd3535f3536c9199ca90f05893e45b
NicolasBuitrago <nicolasb@rolebot.io>
2024-01-22 21:42:34 -0500

    init

---
+++ projects/hola-mundo/src/main.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
+const root = ReactDOM.createRoot(document.getElementById('root') as Element);
+root.render(
+    <React.Fragment>
+        <button>Click me</button>
+    </React.Fragment>
+);
