89e4fee54c922735958f8ab6f905fdb633ec742a
NicolasBuitrago <nicolasb@rolebot.io>
2024-02-12 22:38:59 -0500

    Update .gitignore and App.css, add Square component and update App.jsx

--- projects/tic-tac-toe/src/App.jsx
+++ projects/tic-tac-toe/src/App.jsx
@@ -1,20 +1,73 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
-const board = Array(9).fill(null);
 
-const turns = {
+
+const TURNS = {
   x: 'X',
   o: 'O'
 }
 
+const Square = ({children, isSelected, updateBoard, index}) =>{
+  const className = `square ${isSelected ? 'is-selected' : ''}`
+
+  const handleClick = () => {
+    updateBoard(index)
+  }
+
+  return (
+    <div onClick={handleClick} className={className}>
+      {children}
+    </div>
+  )
+}
+
 function App() {
+  const [board, setBoard] = useState(Array(9).fill(null))
+  const [turn,setTurn] = useState(TURNS.x)
+  const [winner, setWinner] = useState(null)
+
+  const updateBoard = (index) => {
+    
+    if (board[index]) return
+
+    const newBoard = [...board]
+    newBoard[index] = turn
+    setBoard(newBoard)
+
+    const newTurn = turn === TURNS.x ? TURNS.o : TURNS.x
+    setTurn(newTurn)
+  }
+
   
+
   return (
     <main className='board'>
       <h1>Tic Tac Toe</h1>
+      <section className='game'>
+        {
+          board.map((_, index)=>{
+            return (
+              <Square 
+               key={index} 
+               index={index} 
+               updateBoard={updateBoard}
+               >
+                {board[index]}
+              </Square>
+            )
+          })
+        }
+      </section>
+        
+      <section className='turn'>
+      <Square isSelected = {turn === TURNS.x}>
+         {TURNS.x}
+        </Square>
+        <Square isSelected = {turn === TURNS.o}>
+          {TURNS.o}
+        </Square>
+      </section>
      </main> 
   )
    
